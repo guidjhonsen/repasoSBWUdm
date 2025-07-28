660f91aa58e47d5ffa53b9a606b08f67bac5ca5b
Guidjhonsen <guidjhonsen.tu.rey@gmail.com>
2025-07-28 00:10:29 -0400

    Repaso clase 37

---
+++ springboot-web/src/main/java/com/guidjhonsen/curso/springboot/webapp/springboot_web/ValuesConfig.java
@@ -0,0 +1,14 @@
+package com.guidjhonsen.curso.springboot.webapp.springboot_web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+    @PropertySource(value="classpath:values.properties", encoding="UTF-8")
+})
+
+public class ValuesConfig {
+
+}
--- springboot-web/src/main/java/com/guidjhonsen/curso/springboot/webapp/springboot_web/controllers/PathVariableControllers.java
+++ springboot-web/src/main/java/com/guidjhonsen/curso/springboot/webapp/springboot_web/controllers/PathVariableControllers.java
@@ -7,9 +7,12 @@ import com.guidjhonsen.curso.springboot.webapp.springboot_web.models.User;
 import com.guidjhonsen.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 //import org.springframework.web.bind.annotation.RequestParam;
@@ -30,11 +33,31 @@ public class PathVariableControllers {
     private String message;
     
     @Value("${config.listOfValues}")
-    private String[] listOfValues;
+    private //String[] 
+        List<String> listOfValues;
     
     @Value("${config.code}")
     private Integer code;
 
+    @Value("#{'${config.listOfValues}'.toUpperCase().split(',')}")
+    private List<String> valueList;
+
+    
+    @Value("#{'${config.listOfValues}'.toUpperCase()}")
+    private String valueString;
+
+    @Value("#{${config.valuesMap}}")
+    private Map<String,Object> valuesMap;
+
+    @Value("#{${config.valuesMap}.product}")
+    private String product;
+
+    
+    @Value("#{${config.valuesMap}.price}")
+    private Long price;
+
+    @Autowired
+    private Environment environment;
 
     @GetMapping("/baz/{message}")
     public ParamDto baz(@PathVariable() String message) {
@@ -65,7 +88,14 @@ public class PathVariableControllers {
         json.put("username", username);
         json.put("code", code);
         json.put("message", message);
+        json.put("message2", environment.getProperty("config.message"));
+        json.put("code2",environment.getProperty("config.code", Long.class));
         json.put("listOfValues", listOfValues);
+        json.put("valueString", valueString);
+        json.put("valueList", valueList);
+        json.put("valuesMap", valuesMap);
+        json.put("product", product);
+        json.put("price", price);
         return json;
     }
     
