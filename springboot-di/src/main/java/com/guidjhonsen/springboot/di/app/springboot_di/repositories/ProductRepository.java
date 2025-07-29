0c20d98ed805952f9fe0fe809ba9d109cbf59ec8
Guidjhonsen <guidjhonsen.tu.rey@gmail.com>
2025-07-29 19:38:51 -0400

    45. Creando la clase modelo y la clase Repositorio

---
+++ springboot-di/src/main/java/com/guidjhonsen/springboot/di/app/springboot_di/repositories/ProductRepository.java
@@ -0,0 +1,23 @@
+package com.guidjhonsen.springboot.di.app.springboot_di.repositories;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.guidjhonsen.springboot.di.app.springboot_di.models.Product;
+
+public class ProductRepository {
+    private List<Product> data;
+
+    public ProductRepository() {
+        this.data=Arrays.asList(
+            new Product(1L, "Memora 31", 300L),
+            new Product(2L, "Memora cORE I9", 900L),
+            new Product(3L, "tECLADO Razer mini", 180L),
+            new Product(4L, "Motherboard Gigabyte", 490L)
+        );
+    }
+
+    public List<Product> findAll(){
+        return data;
+    }    
+}
