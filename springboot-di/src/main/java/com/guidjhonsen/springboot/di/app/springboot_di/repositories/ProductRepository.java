808c6f44e081288020fdcffe7a82df676c3712da
Guidjhonsen <guidjhonsen.tu.rey@gmail.com>
2025-07-29 19:47:34 -0400

    46. Implementando método findById en la clase Repositoy

--- springboot-di/src/main/java/com/guidjhonsen/springboot/di/app/springboot_di/repositories/ProductRepository.java
+++ springboot-di/src/main/java/com/guidjhonsen/springboot/di/app/springboot_di/repositories/ProductRepository.java
@@ -20,4 +20,8 @@ public class ProductRepository {
     public List<Product> findAll(){
         return data;
     }    
+
+    public Product findById(Long id){
+        return data.stream().filter(p->p.getId().equals(id)).findFirst().orElse(null);
+    }
 }
