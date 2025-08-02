1e5fe7c2be0d8e9f8ed0c9c7e1f0f9a39766e3af
Guidjhonsen <guidjhonsen.tu.rey@gmail.com>
2025-08-01 23:09:12 -0400

    67. Factura y relaciones entre sus componenetes

--- springboot-difactura/src/main/java/com/guidjhonsen/curso/springboot/di/factura/springboot_difactura/models/Item.java
+++ springboot-difactura/src/main/java/com/guidjhonsen/curso/springboot/di/factura/springboot_difactura/models/Item.java
@@ -1,5 +1,22 @@
 package com.guidjhonsen.curso.springboot.di.factura.springboot_difactura.models;
 
 public class Item {
+    private Product product;
+    private Integer quality;
 
+    
+    public Product getProduct() {
+        return product;
+    }
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+    public Integer getQuality() {
+        return quality;
+    }
+    public void setQuality(Integer quality) {
+        this.quality = quality;
+    }
+
+    
 }
