306c6bf5b7bdd7665550e86f86fae06652ebe080
Guidjhonsen <guidjhonsen.tu.rey@gmail.com>
2025-08-06 21:08:21 -0400

    95. Personalizando interceptor para incluir o excluir rutas

--- springboot-interceptor/src/main/java/com/guidjhonsen/curso/springboot/app/interceptor/springbootinterceptor/MVCConfig.java
+++ springboot-interceptor/src/main/java/com/guidjhonsen/curso/springboot/app/interceptor/springbootinterceptor/MVCConfig.java
@@ -1,6 +1,5 @@
 package com.guidjhonsen.curso.springboot.app.interceptor.springbootinterceptor;
 
-import java.util.logging.Handler;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -13,8 +12,10 @@ public class MVCConfig implements WebMvcConfigurer{
     @Autowired
     @Qualifier("timeInterceptor")
     private HandlerInterceptor timeInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(timeInterceptor);
+        registry.addInterceptor(timeInterceptor).addPathPatterns("/app/bar");
+//                .excludePathPatterns("/app/foo", "/app/bar", "/app/baz");
     }
 }
--- springboot-interceptor/src/main/java/com/guidjhonsen/curso/springboot/app/interceptor/springbootinterceptor/controllers/FooController.java
+++ springboot-interceptor/src/main/java/com/guidjhonsen/curso/springboot/app/interceptor/springbootinterceptor/controllers/FooController.java
@@ -7,7 +7,6 @@ import java.util.Collections;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
@@ -16,23 +15,21 @@ public class FooController {
 
     @GetMapping("/foo")
     public Map<String, String> foo() {
-        
-        
-        return Collections.singletonMap("foo", "handlres foo del controlador");
+        return Collections.singletonMap("message", "handlres foo del controlador");
     }
 
     @GetMapping("/bar")
     public Map<String, String> bar() {
         
         
-        return Collections.singletonMap("foo", "handler bar delcontrolador");
+        return Collections.singletonMap("message", "handler bar delcontrolador");
     }
 
     
     @GetMapping("/baz")
     public Map<String, String> baz() {
               
-        return Collections.singletonMap("foo", "handlres foo delcontrolador");
+        return Collections.singletonMap("message", "handlres foo delcontrolador");
     }
 
 }
