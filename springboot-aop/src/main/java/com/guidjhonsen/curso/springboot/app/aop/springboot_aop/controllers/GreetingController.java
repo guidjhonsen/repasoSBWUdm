8c2c18948e772ab5cd5ba77a0b0a8c2dcb6842ce
Guidjhonsen <guidjhonsen.tu.rey@gmail.com>
2025-08-09 17:54:37 -0400

    Añadiendo componenete Service

--- springboot-aop/src/main/java/com/guidjhonsen/curso/springboot/app/aop/springboot_aop/controllers/GreetingController.java
+++ springboot-aop/src/main/java/com/guidjhonsen/curso/springboot/app/aop/springboot_aop/controllers/GreetingController.java
@@ -2,17 +2,22 @@ package com.guidjhonsen.curso.springboot.app.aop.springboot_aop.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.guidjhonsen.curso.springboot.app.aop.springboot_aop.services.GreetingService;
+
 import java.util.Collections;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
 public class GreetingController {
+    @Autowired
+    private GreetingService greetingService;
     @GetMapping("/greeting")
     public ResponseEntity <?> greeting() {
-        return ResponseEntity.ok(Collections.singletonMap("greeting", null));
+        return ResponseEntity.ok(Collections.singletonMap("greeting", greetingService.sayHello("World", "Welcome to Spring Boot AOP!")));
     }
     
 }
