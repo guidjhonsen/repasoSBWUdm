a694fd66ad384d87a71374c0872926cd59ae7fda
Guidjhonsen <guidjhonsen.tu.rey@gmail.com>
2025-08-09 19:31:39 -0400

    109. Advice Around

--- springboot-aop/src/main/java/com/guidjhonsen/curso/springboot/app/aop/springboot_aop/aop/GreetingAspect.java
+++ springboot-aop/src/main/java/com/guidjhonsen/curso/springboot/app/aop/springboot_aop/aop/GreetingAspect.java
@@ -3,9 +3,11 @@ package com.guidjhonsen.curso.springboot.app.aop.springboot_aop.aop;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -53,4 +55,20 @@ public class GreetingAspect {
 
         logger.info("Despues de lanzar la excepcion {} con los argumentos: {}", method, args);
     }
+    @Around("execution(* com.guidjhonsen.curso.springboot.app.aop.springboot_aop.services.*.*(..))")
+    public Object loggerAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+        Object result = null;
+        try {
+            logger.info("Antes de ejecutar el método: {} con los argumentos: {}", method, args);
+            result = joinPoint.proceed();
+            logger.info("Después de ejecutar el método: {} con el resultado: {}", method, result);
+            return result; // Implementar si se desea un comportamiento alrededor del método
+        } catch (Throwable e) {
+            logger.error("Error al registrar antes de ejecutar el método: {}", e.getMessage());
+            throw e;
+        }
+    }
+
 }
